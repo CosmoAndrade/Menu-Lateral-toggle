5bcd3a1a990eeaeaa87585f3b59e0d05dde70d1c
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-03-07 20:20:37 -0300

    Menu Lateral toggle

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// Função para abrir menu lateral
+
+const botaoMenu = document.querySelector('.cabecalho_menu')
+const menu = document.querySelector('.menu-lateral')
+
+botaoMenu.addEventListener('click' , () => {
+    menu.classList.toggle('menu-lateral--ativo')
+})
+
+// ------------------------------------------------------------
+
+
